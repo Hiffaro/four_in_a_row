d489c98a89c924002db9d7e0de62b7b887aa4f8c
Christophe van der Puijl <hiffaro@gmail.com>
2024-11-18 19:47:12 +0100

    Add player logic.

--- src/HumanPlayer.cpp
+++ src/HumanPlayer.cpp
@@ -1,9 +1,24 @@
 #include "HumanPlayer.h"
-#include <cstdlib>
+#include <iostream>
+#include <string>
 
 HumanPlayer::HumanPlayer(Piece playerId) : Player(playerId) {}
 
 void HumanPlayer::doMove() {
-    // Placeholder.
-    std::exit(0);
+    std::cout << "You are player " << (mPlayerId == Piece::PLAYER_1 ? "1" : "2") << "." << std::endl;
+
+    bool madeMove = false;
+    while(!madeMove) {
+        std::cout << "Drop your piece in which column? (1-7)> " << std::flush;
+
+        std::string response;
+        std::getline(std::cin, response);
+
+        if(response.size() > 0) {
+            char c = response[0];
+            if(c >= '1' && c <= '7') {
+                madeMove = mPlayfield->dropPiece(mPlayerId, c - '1');
+            }
+        }
+    }
 }
